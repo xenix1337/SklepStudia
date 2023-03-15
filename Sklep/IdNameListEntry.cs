1d0fbad043c299281d331561d17b4f8e556552d9
xenix1337 <xenix1337gaming@gmail.com>
2023-03-15 11:51:26 +0100

    Dodano okno do rejestrowania nowych produktów w bazie danych

---
+++ Sklep/IdNameListEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep
+{
+    class IdNameListEntry
+    {
+        public int? id { get; private set; }
+        public string name { get; private set; }
+        public IdNameListEntry(int? id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
